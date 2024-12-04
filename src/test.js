b732feb0b451cb4918bed950cd2c84e425210b85
Sneha <sneha@HD-011.local>
2024-12-04 15:25:40 +0530

    data listed

---
+++ src/test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './style.css'
+
+const TestComponent = () => {
+    const [data,setData]=useState();
+    console.log("data",data)
+
+    const getData=()=>{
+            axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken").then((res)=>{
+            setData(res?.data?.meals)
+            console.log("res",res)
+        }).catch((err)=>{
+            console.log(err)
+        })
+
+       
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return(
+<div className='main-div'>
+    {data.map((item)=>{
+        return(
+        <div className='content'>
+           <h1>{item?.strMeal}</h1>
+           <img src={item?.strMealThumb} className='image-div'/> 
+           <span>{item?.strInstructions}</span>
+        </div>
+        )
+    })}
+    
+
+</div>
+    ) 
+}
+
+
+
+export default TestComponent;
